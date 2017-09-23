c6ac12a80d9eeac7d1c9781f8fcf0e09c6bf5b07
Will Fawcett <wfawcett@gmail.com>
2017-09-23 16:44:15 -0600

    starting on js for search

--- public/javascripts/tmdbSearch.js
+++ public/javascripts/tmdbSearch.js
@@ -3,6 +3,7 @@ var _tmdb = {};
 
 _tmdb.doMovieSearch = function(query){
     var restCall = ['https://api.themoviedb.org/3/search/movie?api_key=274472d0b063eec06615cfed6a703b95&language=en-US&query=',encodeURIComponent(query), '&page=1&include_adult=false'].join('');
+    console.log(restCall);
     $.ajax({
         url: restCall,
         timeout: 10000, // 10 second timout
