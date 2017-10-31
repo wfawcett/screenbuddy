68a359e553dd71e4edba05761b775346fbf41890
Will Fawcett <wfawcett@gmail.com>
2017-10-30 21:27:26 -0600

    state changes

--- app/controllers/AccountController.java
+++ app/controllers/AccountController.java
@@ -3,11 +3,11 @@ package controllers;
 import com.google.inject.Inject;
 import io.ebean.Ebean;
 import models.User;
-import play.*;
 import play.data.Form;
 import play.data.FormFactory;
-import play.mvc.*;
+import play.mvc.Result;
 
+import static play.mvc.Controller.flash;
 import static play.mvc.Controller.session;
 import static play.mvc.Results.ok;
 import static play.mvc.Results.redirect;
@@ -18,36 +18,39 @@ public class AccountController {
 
     private User usr;
 
-    public Result index(){
-        if (authorized() == false) bounceThem();
-        return ok(views.html.account.render() );
-
+    public Result index() {
+        if (authorized() == false) return bounceThem();
+        return ok(views.html.account.render());
     }
 
-    public Result login(){
-        return ok(views.html.login.render() );
+    public Result login() {
+        String message = flash("authentication");
+        return ok(views.html.login.render(message));
     }
 
-    public Result authenticate(){
+    public Result authenticate() {
         Form<User> form = formFactory.form(User.class);
         User userFormObject = form.bindFromRequest().get();
         User authorizedUser = User.authCheck(userFormObject.email, userFormObject.hashPass);
-        session("userid", authorizedUser.id.toString());
-        return redirect("/account/");
+        if (authorizedUser == null) {
+            flash("authentication", "Incorrect email or password, please try again");
+            return bounceThem();
+        } else {
+            return redirect("/account/");
+        }
     }
 
-    public boolean authorized(){ // they are a stranger if they are not authenticated.
+    public boolean authorized() {
         String userId = session("userid");
         usr = null;
-        if(userId != null){
+        if (userId != null) {
             usr = Ebean.find(User.class, userId);
         }
         return usr != null;
     }
 
-    public Result bounceThem(){
+    public Result bounceThem() {
         return redirect("/login");
-
     }
 
 }
